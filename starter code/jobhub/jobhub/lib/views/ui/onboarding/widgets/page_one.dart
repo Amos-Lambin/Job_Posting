2e3c204561e8d3dc9bd73f74720103397df07df1
Amos Lambin <amoslambin@gmail.com>
2023-07-28 23:46:51 +0200

    Page One for onboarding screen

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_one.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_one.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:jobhub/constants/app_constants.dart';
 
 class PageOne extends StatelessWidget {
   const PageOne({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: width,
+        height: hieght,
+        color: Color(kDarkPurple.value),
+        child: Column(),
+      ),
+    );
   }
 }
