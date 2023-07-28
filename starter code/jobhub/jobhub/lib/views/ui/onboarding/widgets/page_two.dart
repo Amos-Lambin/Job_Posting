20219e47e5e9792731bb1f2df41253dde0b5802a
Amos Lambin <amoslambin@gmail.com>
2023-07-29 00:44:18 +0200

    Page two

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_two.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_two.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
 
 import '../../../../constants/app_constants.dart';
 
@@ -11,7 +13,15 @@ class PageTwo extends StatelessWidget {
         body: Container(
       width: width,
       height: hieght,
-      color: Color(kDarkPurple.value),
+      color: Color(kDarkBlue.value),
+      child: Column(
+        children: [
+          const HeightSpacer(size: 65),
+          Padding(
+            padding: EdgeInsets.all(8.h),
+          ),
+        ],
+      ),
     ));
   }
 }
