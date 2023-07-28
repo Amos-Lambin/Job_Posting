0f2f7b3064eab5d1dabb5d7f8dbf249258a4a50b
Amos Lambin <amoslambin@gmail.com>
2023-07-29 00:34:38 +0200

    Page one

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_one.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_one.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jobhub/constants/app_constants.dart';
+import 'package:jobhub/views/common/exports.dart';
 import 'package:jobhub/views/common/height_spacer.dart';
+import 'package:jobhub/views/common/reusable_text.dart';
 
 class PageOne extends StatelessWidget {
   const PageOne({super.key});
@@ -14,10 +17,28 @@ class PageOne extends StatelessWidget {
         color: Color(kDarkPurple.value),
         child: Column(
           children: [
-            HeightSpacer(
+            const HeightSpacer(
               size: 70,
             ),
-            Image.asset("")
+            Image.asset("assets/images/page1.png"),
+            const HeightSpacer(size: 40),
+            Column(
+              children: [
+                ReusableText(
+                  text: "Find Your Dream Job",
+                  style: appstyle(30, kLight, FontWeight.w500),
+                ),
+                const HeightSpacer(size: 10),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0.w),
+                  child: Text(
+                    "We help you find your dream job according to your skillset, location and preference to build your career",
+                    textAlign: TextAlign.center,
+                    style: appstyle(14, Color(kLight.value), FontWeight.normal),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
