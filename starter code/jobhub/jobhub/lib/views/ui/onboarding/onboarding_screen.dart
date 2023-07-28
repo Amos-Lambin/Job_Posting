20adcbe1856068fe2b864bbb1f5feb6d11aa67fc
Amos Lambin <amoslambin@gmail.com>
2023-07-28 23:41:23 +0200

    onboarding Screen

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/onboarding_screen.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/onboarding_screen.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:jobhub/views/common/exports.dart';
-
+import 'package:jobhub/views/ui/onboarding/widgets/page_one.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_three.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_two.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ReusableText(text: "OnBoardingScreen", style: appstyle(30, Color(kDark.value), FontWeight.bold),),
+    return const Scaffold(
+      body: Stack(
+        children: [
+          PageOne(),
+          PageTwo(),
+          PageThree(),
+        ],
       ),
     );
   }
