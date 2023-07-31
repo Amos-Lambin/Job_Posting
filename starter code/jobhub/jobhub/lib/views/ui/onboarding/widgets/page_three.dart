cc359f155cb933a8bfec45aa96436df075eea53a
Amos Lambin <amoslambin@gmail.com>
2023-08-01 00:02:16 +0200

    Page three

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_three.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jobhub/constants/app_constants.dart';
 import 'package:jobhub/views/common/app_style.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
 import 'package:jobhub/views/common/reusable_text.dart';
 
 class PageThree extends StatelessWidget {
@@ -12,7 +14,7 @@ class PageThree extends StatelessWidget {
         body: Container(
             width: width,
             height: hieght,
-            color: Color(kLight.value),
+            color: Color(kLightBlue.value),
             child: Column(
               children: [
                 Image.asset("assets/image/page3.png"),
@@ -20,6 +22,16 @@ class PageThree extends StatelessWidget {
                   text: "Welcome to JobHunt",
                   style: appstyle(30, Color(kLight.value), FontWeight.w500),
                 ),
+                const HeightSpacer(size: 15),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.w),
+                  child: Text(
+                    "We help you find your dream job according to your skillset, location and preference to build your career",
+                    textAlign: TextAlign.center,
+                    style: appstyle(14, Color(kLight.value), FontWeight.normal),
+                  ),
+                ),
+                const HeightSpacer(size: 20)
               ],
             )));
   }
