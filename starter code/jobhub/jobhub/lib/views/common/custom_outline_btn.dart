5ffdab220cdbf66b3509974de241bbb8f15b678b
Amos Lambin <amoslambin@gmail.com>
2023-08-01 00:18:56 +0200

    Custom outline button done

--- starter code/jobhub/jobhub/lib/views/common/custom_outline_btn.dart
+++ starter code/jobhub/jobhub/lib/views/common/custom_outline_btn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobhub/views/common/exports.dart';
 
 class CustomOutlineBtn extends StatelessWidget {
   const CustomOutlineBtn(
@@ -18,6 +19,25 @@ class CustomOutlineBtn extends StatelessWidget {
   final Color color2;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: color2,
+          border: Border.all(
+            width: 1,
+            color: color,
+          ),
+        ),
+        child: Center(
+          child: ReusableText(
+            text: text,
+            style: appstyle(16, color, FontWeight.w600),
+          ),
+        ),
+      ),
+    );
   }
 }
