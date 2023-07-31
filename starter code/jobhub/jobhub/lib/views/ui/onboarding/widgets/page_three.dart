7b003b84fcfb33db4fe70a4321dc73b46f68a3a2
Amos Lambin <amoslambin@gmail.com>
2023-07-31 23:53:31 +0200

    Page three

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_three.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/widgets/page_three.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jobhub/constants/app_constants.dart';
+import 'package:jobhub/views/common/app_style.dart';
+import 'package:jobhub/views/common/reusable_text.dart';
 
 class PageThree extends StatelessWidget {
   const PageThree({super.key});
@@ -14,6 +16,10 @@ class PageThree extends StatelessWidget {
             child: Column(
               children: [
                 Image.asset("assets/image/page3.png"),
+                ReusableText(
+                  text: "Welcome to JobHunt",
+                  style: appstyle(30, Color(kLight.value), FontWeight.w500),
+                ),
               ],
             )));
   }
