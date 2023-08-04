df4ad2270808cbdda993c63e613a135684d94a77
Amos Lambin <amoslambin@gmail.com>
2023-08-04 03:09:57 +0200

    Onboarding Screen
    Added two gesture detcetors for next and skip

--- starter code/jobhub/jobhub/lib/views/ui/onboarding/onboarding_screen.dart
+++ starter code/jobhub/jobhub/lib/views/ui/onboarding/onboarding_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jobhub/constants/app_constants.dart';
 import 'package:jobhub/controllers/exports.dart';
+import 'package:jobhub/views/common/exports.dart';
 import 'package:jobhub/views/ui/onboarding/widgets/page_one.dart';
 import 'package:jobhub/views/ui/onboarding/widgets/page_three.dart';
 import 'package:jobhub/views/ui/onboarding/widgets/page_two.dart';
@@ -64,6 +66,42 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
                         ),
                       ),
               ),
+              Positioned(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.w,
+                      vertical: 30.h,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          child: ReusableText(
+                            text: "Skip",
+                            style: appstyle(
+                              16,
+                              Color(kLight.value),
+                              FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          child: ReusableText(
+                            text: "Next",
+                            style: appstyle(
+                              16,
+                              Color(kLight.value),
+                              FontWeight.w500,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           );
         },
